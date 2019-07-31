d7c56b1d72ae4cc1d6573a79dc1061e4c1892d62
EC2 Default User <ec2-user@ip-172-31-25-151.us-east-2.compute.internal>
2019-07-31 19:42:14 +0000

    message

---
+++ app/routes.py
@@ -0,0 +1,60 @@
+from app import app
+from flask import render_template, request, session, url_for, redirect
+from app.models import model, formopener
+from flask_pymongo import PyMongo
+
+app.config['MONGO_DBNAME'] = 'finalProject'
+app.config['MONGO_URI'] = 'mongodb+srv://aamin:8FJctF4pbI4BlECv@cluster0-ybive.mongodb.net/test?retryWrites=true&w=majority'
+mongo = PyMongo(app)
+collection = mongo.db.dataPoints
+points = list(collection.find({}))
+
+
+
+@app.route('/')
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    
+    if 'username' not in session:
+        session['username'] = 'newUser'
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+        if existing_user is None:
+            users.insert({'name' : request.form['username'], 'password' : request.form['password']})
+            session['username'] = request.form['username']
+            namedisplay = session['username']
+            return redirect(url_for('index'))
+        else:
+            return redirect(url_for('index'))
+    return render_template('signup.html')
+@app.route('/index')
+def index():
+    #reset session variables
+    return render_template('index.html')
+    
+@app.route('/dataEntry', methods= ['GET','POST'])
+def dataEntry():
+    userdata = request.form
+    xName = userdata["X-axis"]
+    yName = userdata["Y-axis"]
+    axes = mongo.db.axes
+    axes.insert({"xName":xName,"yName":yName})
+    return render_template('dataEntry.html', xName = xName, yName = yName)
+    
+app.secret_key = 'totallySecret'    
+@app.route('/data', methods=['GET','POST'])
+def data():
+    pointdata = request.form
+    #print(pointdata)
+    dataPoints = mongo.db.dataPoints
+    xVal = float(pointdata["X-point"])
+    yVal = float(pointdata["Y-point"])
+    dataPoints.insert({"xVal":xVal, "yVal":yVal})
+    xName = collection["xName"]
+    yName = collection["yName"]
+    return render_template('dataEntry.html', xName = xName, yName = yName)
+    
+@app.route('/results', methods=['GET','POST'])
+def results():
+    return render_template('results.html')
